b50f74fefed3b3579fe651199e56750b3fe4330b
Karthik <Karthigaming121@gamil.com>
2025-10-26 00:10:39 +0530

    fixed the bug

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ async def solve(file:UploadFile=File(...)):
     
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
